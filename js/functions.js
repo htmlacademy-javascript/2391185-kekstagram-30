eb5f251ee99b64cadce57dccab3c4022ad1d422c
PTkavhev <tkapavl@gmail.com>
2023-11-15 19:03:01 +0300

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -34,3 +34,25 @@ extractNumbers('1 кефир, 0.5 батона');
 extractNumbers('агент 007');
 extractNumbers('а я томат');
 extractNumbers('-1');
+
+const MINUTES_IN_HOUR = 60;
+
+function getTimeInMinutes (time) {
+  const timeSplit = time.split(':');
+  return (parseInt(timeSplit[0], 10) * MINUTES_IN_HOUR + parseInt(timeSplit[1], 10));
+}
+
+function isTimeInWorkScope (workTimeStart, workTimeEnd, meetingTimeStart, meetingDuration) {
+  if (getTimeInMinutes(meetingTimeStart) >= getTimeInMinutes(workTimeStart)) {
+    if (getTimeInMinutes(workTimeEnd) >= (getTimeInMinutes(meetingTimeStart) + meetingDuration)){
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(isTimeInWorkScope('08:00', '17:30', '14:00', 90)); // true
+console.log(isTimeInWorkScope('8:0', '10:0', '8:0', 120)); // true
+console.log(isTimeInWorkScope('08:00', '14:30', '14:00', 90)); // false
+console.log(isTimeInWorkScope('14:00', '17:30', '08:0', 90)); // false
+console.log(isTimeInWorkScope('8:00', '17:30', '08:00', 900)); // false
