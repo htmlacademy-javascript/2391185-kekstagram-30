4409f533fd391a123a7b7dc5644e02e0b6514192
PTkavhev <tkapavl@gmail.com>
2023-11-24 22:08:29 +0300

    Убран вывод в консоль

--- js/main.js
+++ js/main.js
@@ -2,5 +2,4 @@ import { generatedDescriptions } from './data';
 import { drawThumbnails } from './draw_thumbnail.js';
 
 const descr = generatedDescriptions();
-console.log(descr[0].comments);
 drawThumbnails(descr);
