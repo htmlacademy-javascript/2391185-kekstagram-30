5536ecd5b836231a220025385ab581f622957710
PTkavhev <tkapavl@gmail.com>
2023-11-26 20:32:01 +0300

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-import { generatedDescriptions } from './data';
+//import { generatedDescriptions } from './data';
+import { getData } from './fetchApi.js';
 import { drawThumbnails } from './draw_thumbnail.js';
-import './uploadPicture.js';
+import { showAlert } from './utility.js';
+import { setFormAction } from './uploadPictures/uploadPictureForm.js';
 
-const descr = generatedDescriptions();
-drawThumbnails(descr);
+getData()
+  .then((pictures) => {
+    drawThumbnails(pictures);
+  })
+  .catch((err) => {
+    showAlert(err);
+  });
+
+setFormAction();
---
+++ js/uploadPictures/uploadPictureForm.js
@@ -0,0 +1,76 @@
+import { isEscape } from '../utility.js';
+import { scaleBiggerHandler, scaleSmallerHandler, scaleBiggerHandlerRemove, scaleSmallerHandlerRemove, DefaultPreviewScaleHandler} from './changeScale.js';
+import { addValidatorsPristine, validateFormPristine, resetValidatorsPristine } from './addValidators.js';
+import {effectChangeHandler, effectChangeHandlerRemove, resetEffects } from './implementFilter.js';
+import { sendData } from '../fetchApi.js';
+
+const SubmitButtonText = {
+  IDLE: 'Опубликовать',
+  SENDING: 'Публикую...',
+};
+
+const uploadPictureFormElement = document.querySelector('.img-upload__form');
+const uploadPictureElement = document.querySelector('.img-upload__input');
+const editPictureFormElement = document.querySelector('.img-upload__overlay');
+const closePictureFormElement = document.querySelector('.img-upload__cancel');
+const submitButtonElement = document.querySelector('.img-upload__submit');
+
+const effectSliderContainerElement = document.querySelector('.img-upload__effect-level');
+
+const blockSubmitButton = () => {
+  submitButtonElement.disabled = true;
+  submitButtonElement.textContent = SubmitButtonText.SENDING;
+};
+
+const unblockSubmitButton = () => {
+  submitButtonElement.disabled = false;
+  submitButtonElement.textContent = SubmitButtonText.IDLE;
+};
+
+const onUploadPictureForm = (evt) => {
+  evt.preventDefault();
+  if (validateFormPristine()){
+    blockSubmitButton();
+    const formData = new FormData(evt.target);
+    sendData(formData);
+  }
+};
+
+const onUploadPictureChange = () => {
+  editPictureFormElement.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  effectSliderContainerElement.classList.add('hidden');
+  effectChangeHandler();
+  closePictureFormElement.addEventListener('click', closePictureForm);
+  document.addEventListener('keydown', onDocumentKeydown);
+  uploadPictureFormElement.addEventListener('submit', onUploadPictureForm);
+  addValidatorsPristine();
+  scaleBiggerHandler();
+  scaleSmallerHandler();
+};
+
+function closePictureForm () {
+  editPictureFormElement.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  DefaultPreviewScaleHandler();
+  uploadPictureElement.value = '';
+  uploadPictureFormElement.removeEventListener('submit', onUploadPictureForm);
+  document.removeEventListener('keydown', onDocumentKeydown);
+  resetEffects();
+  effectChangeHandlerRemove();
+  scaleBiggerHandlerRemove();
+  scaleSmallerHandlerRemove();
+  resetValidatorsPristine();
+}
+
+function onDocumentKeydown (evt) {
+  if(isEscape(evt) && !evt.target.closest('.img-upload__field-wrapper') &&
+  !(document.body.querySelector('.error') || document.body.querySelector('.success'))){
+    evt.preventDefault();
+    closePictureForm();
+  }
+}
+
+const setFormAction = () => uploadPictureElement.addEventListener('change', onUploadPictureChange);
+
+export { setFormAction, unblockSubmitButton, closePictureForm };
